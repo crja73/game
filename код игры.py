68a585f66bf3ee49721d27206ef96bdcbe490afb
crja73 <57744842+crja73@users.noreply.github.com>
2020-01-09 22:52:54 +0300

    Add files via upload

--- код игры.py
+++ код игры.py
@@ -6,13 +6,12 @@ import random
 BLUE = (0, 0, 255)
 
 pygame.init()
-size = width, height = 900, 650
+size = width, height = 450, 650
 screen = pygame.display.set_mode(size)
 screen.fill(BLUE)
 x = 0
 y =550
-x2 = 0
-y2 = 500
+
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -32,67 +31,173 @@ def load_image2(name, colorkey=None):
         image = image.convert_alpha()
     return image
 
+def load_image3(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname).convert_alpha()
+    if colorkey is not None:
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+def load_image4(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname).convert_alpha()
+    if colorkey is not None:
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину    
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')    
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
 class Tile(pygame.sprite.Sprite):
-	image = load_image2('препятствие 1.2.png')
+	image = load_image2('огр.png')
+
 	def __init__(self, group):
 		super().__init__(group)
 		self.image = Tile.image
 		self.rect = self.image.get_rect()
+		self.mask = pygame.mask.from_surface(self.image)
+
 
 	def update(self, event):
 		self.rect.x = event[0]
+		self.rect.y = event[1]
+
+class Dragon(pygame.sprite.Sprite):
+	image = load_image4('дракончик.png')
+
+	def __init__(self, group):
+		super().__init__(group)
+		self.image = Dragon.image
+		self.rect = self.image.get_rect()
+
+	def update(self, event):
+		self.rect.x = event[0]
+		self.rect.y = event[1]
+
+
 
 class Bird(pygame.sprite.Sprite):
     image = load_image("new_geralt.png")
 
     def __init__(self, group):
         super().__init__(group)
+        self.count_jump = 15
+        self.jump_flag = False
         self.image = Bird.image
         self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+
 
     def update(self, event):
-        self.rect.x = event[0]
-        self.rect.y = event[1]
+    	if not pygame.sprite.collide_mask(self, tile):
+    		self.rect.x = event[0]
+    		self.rect.y = event[1]
+    		if self.count_jump >= -15:
+    			self.rect.y -= self.count_jump / 2
+    			self.count_jump -= 1
+    		else: 
+    			if self.rect.y < 550:   
+    				self.rect.y = min(550, (self.rect.y - self.count_jump / 2))
+    				self.count_jump -= 1
+    			else:
+    				self.count_jump = 15  
+    				self.jump_flag = False
+
+    		
+
+
+class Bullet(pygame.sprite.Sprite):
+	image = load_image3('bullet.png')
+
+	def __init__(self, group):
+		super().__init__(group)
+		self.image = Bullet.image
+		self.rect = self.image.get_rect()
+
+	def update(self, event):
+		self.rect.x = event[0]
+		self.rect.y = event[1]
 
 
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+dragon_group = pygame.sprite.Group()
 
 Tile(tiles_group)
 Bird(all_sprites)
-count_jump = 20
-jump_flag = False
+Bullet(bullets)
+Dragon(dragon_group)
+tile = Tile(tiles_group)
 
-x_pos = 800
+
+
+diaposon_of_mob = []
+diaposon_of_mob_x = []
+for i in range(420, 650):
+	diaposon_of_mob.append(i)
+for j in range(50, 150):
+	diaposon_of_mob_x.append(j)
+x_pos = 450
 y_pos = 500
+xd = 450
+yd = 200
+height = []
+bullet_flag = False
+simple_flag = False
+
+x2 = x
 
 running = True
-while running:
-    all_sprites.update([x, y])
-    tiles_group.update([x_pos, y_pos])
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    x_pos -= 1
-    tiles_group.update([x_pos])
-    if event.type == pygame.KEYDOWN:    
-        if event.key == pygame.K_SPACE:    # это проверка на нажатие пробела
-        	jump_flag = True      #объявляем прыжок и ставим коэфициент ускорения прыжка на максимум
-        	count_jump = 20
-    if jump_flag == True:   #если флаг истенен, а знчит мы нажали на пробел, то мы совершаем прыжок
-    	if count_jump >= -20:
-    		y -= count_jump / 2   #эти две строчки описывают прыжок вверх, без возвращения назад
-    		count_jump -= 1       #они нужны для того, чтобы прожок был не резким, а поавным и с ускорением
-    	else:
-    		if y < 550:    #это проверка на положоние спрайта относительно земли
-    			y = min(550, (y - count_jump / 2))   #эти строчки возвращают птицу строго на замлю, птица всегда будет стремиться падать на землю
-    			count_jump -= 1
-    		else:
-    			count_jump = 20   #если мы все же приземлились на землю, то коэфициент становится прежним, а режим прыжка отменяется
-    			jump_flag = False
- 
-    screen.fill(BLUE)
-    all_sprites.draw(screen)
-    tiles_group.draw(screen)
-    pygame.display.flip()
-    pygame.time.wait(10)   #обновляем экран и ставим подходящую скорость
+while running: #генрацию препятствий можно сделать с помощью чтения из файла
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+	x_pos -= 2    #это движение припятствия
+	tiles_group.update([x_pos, y_pos])
+	if x_pos < -110:
+		x_pos = 400
+		y_pos = 500
+	xd -= 2
+	dragon_group.update([xd, yd])
+	if xd < - 220:
+		xd = 450
+		yd = 200
+	if event.type == pygame.KEYDOWN:
+		if event.key == pygame.K_SPACE:    # это проверка на нажатие пробела
+		    all_sprites.update([x, y])
+		elif event.key == pygame.K_UP:
+			y2 = y
+			x2 = x + 50
+			bullet_flag = True
+	
+
+
+
+	if bullet_flag:
+		x2 += 1
+		bullets.update([x2, y2])
+		 
+
+	screen.fill(BLUE)
+	all_sprites.draw(screen)
+	tiles_group.draw(screen)
+	bullets.draw(screen)
+	dragon_group.draw(screen)
+	pygame.display.flip()
+	pygame.time.wait(8)   #обновляем экран и ставим подходящую скорость
